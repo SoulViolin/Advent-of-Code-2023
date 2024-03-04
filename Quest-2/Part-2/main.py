344a4bba44320be5fe5d385fbfac5976b8dbc5a6
SoulViolin <89086268+SoulViolin@users.noreply.github.com>
2024-03-04 19:22:32 +0300

    Remove unnecessary code

--- Quest-2/Part-2/main.py
+++ Quest-2/Part-2/main.py
@@ -1,31 +1,19 @@
-def main(Mred, Mgreen, Mblue):
+def main():
     appropriateValue = []
 
     fileName = "input.txt"
-    file = readFile(fileName)
+    lines = readFile(fileName)
 
-    for el in file:
-        flag = True
-
+    for el in lines:
         name, count = el.split(": ")[0].split(" ")
         games = [{item.split()[1]: int(item.split()[0]) for item in game.split(", ")} for game in el.split(": ")[1].split("; ")]
 
-        MGred = 1
-        MGgreen = 1
-        MGblue = 1
-
-        for game in games:
-            Gred = game.get("red", 0)
-            Ggreen = game.get("green", 0)
-            Gblue = game.get("blue", 0)
-
-            MGred = max(MGred, Gred)
-            MGgreen = max(MGgreen, Ggreen)
-            MGblue = max(MGblue, Gblue)
+        MGred = max([game.get("red", 0) for game in games] + [1])
+        MGgreen = max([game.get("green", 0) for game in games] + [1])
+        MGblue = max([game.get("blue", 0) for game in games] + [1])
         
-        if flag: 
-            total = MGred * MGgreen * MGblue
-            appropriateValue.append(total)
+        total = MGred * MGgreen * MGblue
+        appropriateValue.append(total)
     
     total = sum(appropriateValue)
     print(total)
@@ -35,10 +23,4 @@ def readFile(name):
         return f.readlines()
 
 if __name__ == '__main__':
-    try:
-        red, green, blue = input("Maximum number of cubes: Red, Green, Blue\n").split(", ")
-    except:
-        red, green, blue = 12, 13, 14
-
-    print(f"Maximum number of cubes: Red-{red}, Green-{green}, Blue-{blue}")
-    main(red, green, blue)
+    main()
