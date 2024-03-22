0a882f8c54275635b7ed567f93d74cbe870fb750
SoulViolin <89086268+SoulViolin@users.noreply.github.com>
2024-03-22 14:39:00 +0300

    Solution of the second part of the 9th day

---
+++ Quest-9/Part-2/main.py
@@ -0,0 +1,29 @@
+import sys
+
+def main(file_name):
+    lines = read_file(file_name)
+    histories = [calculate_history([int(el) for el in line.rstrip().split()]) for line in lines]
+    first_el_histories = [[sublist[0] for sublist in history] for history in histories]
+    print(sum([history[0] - sum(history[i] * ((-1) ** (i+1)) for i in range(1, len(history))) for history in first_el_histories]))
+
+def read_file(name):
+    with open(file=name, mode="r", encoding="utf-8") as f:
+        return f.readlines()
+
+def calculate_history(sequence):
+    """
+    >>> calculate_history([0, 3, 6, 9, 12, 15])
+    [[0, 3, 6, 9, 12, 15], [3, 3, 3, 3, 3], [0, 0, 0, 0]]
+    """
+    history = [sequence]
+    while len(sequence) > 1 and not all(x == 0 for x in sequence):
+        sequence = [sequence[i + 1] - sequence[i] for i in range(len(sequence) - 1)]
+        history.append(sequence)
+    return history
+
+if __name__ == '__main__':
+    try:
+        file_name = sys.argv[1]
+    except:
+        file_name = "Quest-9/input.txt"
+    main(file_name)
